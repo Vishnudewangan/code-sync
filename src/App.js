974d8cfa4e75ddd7461da20218147fd4db79c804
Vishnudewangan <dewanganvish1721@gmail.com>
2022-08-08 23:30:08 +0530

    Avatar Added

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/editor/roomId" element={<EditorPage />}></Route>
+          <Route path="/editor/:roomId" element={<EditorPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import Client from "../components/Client";
 
 const EditorPage = () => {
-  return  <div>Editor Page</div>
-  
-}
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Rakesh K" },
+    { socketId: 2, username: "Jonn Doe" },
+  ]);
 
-export default EditorPage
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="editorWrap">Editor goes here...</div>
+    </div>
+  );
+};
+
+export default EditorPage;
