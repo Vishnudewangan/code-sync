32baba57ded86f9598f73feabc8f6e276764b77e
Vishnudewangan <dewanganvish1721@gmail.com>
2022-08-08 20:47:13 +0530

    unique user id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = () => {
+    const[roomId, setRoomId] = useState('');
+    const[username, setUsername] = useState('');
+
+    const createNewRoom = (e)=> {
+        e.preventDefault();
+        const id = uuidv4();
+        setRoomId(id);
+       
+    };
   return (
+    
+
     <div className='homePageWrapper'>
         
         <div className='formWrapper'>
-            <img src='/code-sync.png' alt='code-sync-logo' />
+            <img className='homePageLogo' src='/code-sync.png' alt='code-sync-logo' />
             <h4 className='mainLabel'>Paste invitation ROOM ID</h4>
 
             <div className='inputGroup'>
@@ -14,26 +26,30 @@ const Home = () => {
                 type="text"
                 className='inputBox'
                 placeholder='Room ID'
+                onChange={(e)=>setRoomId(e.target.value)}
+                value={roomId}
                 
                 />
 
             <input 
                 type="text"
                 className='inputBox'
-                placeholder='Room ID'
+                placeholder='USERNAME'
+                onChange={(e)=>setUsername(e.target.value)}
+                value={username}
                 
                 />
                 <button className="btn joinBtn">Join</button>
                 <span className='createInfo'>
                     If you don't have an invite then create &nbsp;
-                    <a href='' className='createNewBtn'>new room</a>
+                    <a onClick={createNewRoom} href='' className='createNewBtn'>new room</a>
                 </span>
 
             </div>
 
         </div>
         <footer>
-            <h4>Build with 💛 &nbsp; by {' '}
+            <h4>Build with 💛 &nbsp; by &nbsp;
                 <a href='https://github.com/Vishnudewangan/'>Vishnu</a></h4>
         </footer>
 
